eef4036d8f64225dcd654fd2a99309c0a24846f6
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2024-06-09 07:35:41 +0200

    Temporarily disable nix-direnv overlay

--- modules/overlays/nix-direnv/default.nix
+++ modules/overlays/nix-direnv/default.nix
@@ -1,14 +1,14 @@
 { ... }:
 
 final: prev: {
-  nix-direnv = prev.nix-direnv.overrideAttrs (attrs: {
-    prePatch = ''
-      echo PATCHING
-      pwd
-      ls -alh
-    '';
-    patches = (attrs.patches or [ ]) ++ [
-      ./nvd.patch
-    ];
-  });
+  # nix-direnv = prev.nix-direnv.overrideAttrs (attrs: {
+  #   prePatch = ''
+  #     echo PATCHING
+  #     pwd
+  #     ls -alh
+  #   '';
+  #   patches = (attrs.patches or [ ]) ++ [
+  #     ./nvd.patch
+  #   ];
+  # });
 }
